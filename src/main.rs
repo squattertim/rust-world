93fe934087d9f01b0c52e06857410c0955ed78ff
Tomas Richta <tomas.richta@gmail.com>
2023-10-16 21:32:54 +0200

    Homework 2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,53 @@
+use std::env;
 use std::io;
+use slug::slugify;
 
 fn main() {
-    // Print a message to ask for the user's name
-    println!("Enter your name:");
 
-    // Create a new String to store the user input
-    let mut name = String::new();
+    let args: Vec<String> = env::args().collect();
 
-    // Read the user input and store it in the 'name' variable
-    io::stdin().read_line(&mut name).expect("Failed to read line");
+    match args.as_slice() {
+        [ref name] => {
+            println!("Running '{}'. Please, pass some arguments!", name);
+        },
+        [_, ref string] => {
 
-    // Trim the trailing newline character from the input
-    let name = name.trim();
+            println!("Enter your input:");
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    // Print a greeting message with the user's name
-    println!("Hello, {}!", name);
+            match string.as_str() {
+                "lowercase" => lowercase(input),
+                "uppercase" => uppercase(input),
+                "nospaces" => nospaces(input),
+                "slugify" => slug(input),
+                _ => {
+                    println!("Error: unknown command");
+                },
+            }
+        },
+        _ => {
+            println!("Too many args!");
+        }
+    }
+}
+
+fn print_result(result: String) {
+    println!("Result: {}", result);
+}
+
+fn uppercase(string: String) {
+    print_result(string.to_uppercase());
+}
+
+fn lowercase(string: String) {
+    print_result(string.to_lowercase());
+}
+
+fn nospaces(string: String) {
+    print_result(string.replace(" ", ""));
+}
+
+fn slug(string: String) {
+    print_result(slugify(string));
 }
