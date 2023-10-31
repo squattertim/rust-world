5175863f466a1503490718915ae8be3f09748250
Tomáš Richta <tomas.richta@gmail.com>
2023-10-31 11:19:51 +0100

    HW#7 Communicating threads

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-
+use std::{io, thread, sync::mpsc};
 
 fn main() {
+    let (tx, rx) = mpsc::channel();
 
+    thread::spawn(move || {
+        loop {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read line");
+            tx.send(input).unwrap();
+        }
+    });
+
+    thread::spawn(move || {
+        for received in rx {
+            println!("{}", received);
+        }
+    }).join().unwrap();
 }
